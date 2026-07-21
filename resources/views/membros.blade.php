a46b98fc20786c1a65983dcc8f52ee352a0f65b0
João Pedro Santana dos Anjos <joaoanjossantana@gmail.com>
2026-07-21 11:55:31 -0300

    feat: implementa primeira rota, controller e view de membros

---
+++ resources/views/membros.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema para Igrejas</title>
+</head>
+<body>
+
+    <h1>Cadastro de Membros</h1>
+
+    <p>Minha primeira página em Laravel 🚀</p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MembroController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/membros', [MembroController::class, 'index'
+]);
