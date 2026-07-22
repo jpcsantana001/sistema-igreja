8be880a163b7c1645467512d0e76fb59984bd367
João Pedro Santana dos Anjos <joaoanjossantana@gmail.com>
2026-07-22 11:12:43 -0300

    feat:adiciona formulario de cadastro de membro

--- resources/views/membros.blade.php
+++ resources/views/membros.blade.php
@@ -2,13 +2,31 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>Sistema para Igrejas</title>
+    <title>Cadastro de Membros</title>
 </head>
 <body>
 
     <h1>Cadastro de Membros</h1>
 
-    <p>Minha primeira página em Laravel 🚀</p>
+    <form action="/membros" method="POST">
+
+        @csrf
+
+        <label>Nome:</label><br>
+        <input type="text" name="nome"><br><br>
+
+        <label>Telefone:</label><br>
+        <input type="text" name="telefone"><br><br>
+
+        <label>Endereço:</label><br>
+        <input type="text" name="endereco"><br><br>
+
+        <label>Data de nascimento:</label><br>
+        <input type="date" name="data_nascimento"><br><br>
+
+        <button type="submit">Salvar</button>
+
+    </form>
 
 </body>
 </html>
